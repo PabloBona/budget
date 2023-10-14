a14f6d4c56f614b5cd96137793b7d5c3609589c3
PabloBona <pablobonasera@gmail.com>
2023-10-14 19:57:36 -0300

    Update models and migrations

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,5 @@ class User < ApplicationRecord
 
   # Validations
   validates :name, presence: true, length: { maximum: 50 }
-  validates :surname, presence: true, length: { maximum: 50 }
   validates :email, presence: true, length: { maximum: 255 }
 end
--- db/migrate/20231014130252_add_atr_users.rb
+++ db/migrate/20231014130252_add_atr_users.rb
@@ -1,6 +1,5 @@
 class AddAtrUsers < ActiveRecord::Migration[7.0]
   def change
-    add_column :users, :surname, :string
     add_column :users, :phone, :string
     add_column :users, :birthday, :date
     add_column :users, :photo, :string
