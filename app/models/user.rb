64cb3d2366c50e87ae3678406ed8a491fe23e9af
PabloBona <pablobonasera@gmail.com>
2023-10-16 09:46:26 -0300

    Update models -credit_card - as requiered Update schema -phone and birthday-

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,4 @@ class User < ApplicationRecord
   # Validations
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, length: { maximum: 255 }
-  validates :credit_card, length: { is: 16 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -27,10 +27,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_14_130252) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
-    t.string "phone"
-    t.date "birthday"
     t.string "photo"
-    t.string "credit_card"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
